34e6e49984aeb7b04c330aa3eea7f7fec348fb3f
Julie <jq.nikitina@gmail.com>
2022-01-23 14:06:26 +0700

    Переход на свою апи

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,10 +1,8 @@
-const BASE_ROUTE = 'https://mesto.nomoreparties.co/v1/cohort-28';
-const TOKEN = '0760ddfd-b432-4756-af57-8cde608b855b';
+const BASE_ROUTE = 'https://api.onemoreproject.nomoredomains.rocks';
 
 export const AUTORIZATION_PARAMS = {
   baseRoute: BASE_ROUTE,
   headers: {
-    authorization: TOKEN,
     'Content-Type': 'application/json'
   }
 }
